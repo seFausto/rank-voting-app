a3d461b0c791c89cc211833fe768d79ca2d2e238
Fausto Ruvalcaba <se.fausto@outlook.com>
2021-11-03 16:58:15 -0700

    Add history of votes

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -10,8 +10,20 @@ export class LandingComponent implements OnInit {
   constructor() { }
 
   voteId: string;
-  
+  votes: string[];
+
   ngOnInit(): void {
+
+    this.votes = [];
+
+    let theCookies = document.cookie.split(';');
+
+    for (var i = 0; i < theCookies.length; i++) {
+      let pastVoteId = theCookies[i].split('=');
+
+      this.votes.push(pastVoteId[0].replace(/\s/g, ""));
+
+    }
   }
 
 }
--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { CandidateService } from '../services/candidates.service';
-import { Observable, of } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -11,15 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ResultsComponent implements OnInit {
 
-  constructor(private candidateService: CandidateService, 
+  constructor(private candidateService: CandidateService,
     private route: ActivatedRoute) { }
 
-  
+
   resultArray: string[];
   voteId: string;
-  
+
   ngOnInit(): void {
-    
+
     this.route.queryParams
       .subscribe(params => {
         this.voteId = params.id;
