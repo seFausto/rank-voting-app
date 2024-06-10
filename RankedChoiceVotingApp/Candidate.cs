467752feb5eaa29fac3f2e59ac3b2fb88a83056f
Fausto Ruvalcaba <se.fausto@outlook.com>
2024-06-09 22:55:24 -0700

    Initial Commit

---
+++ RankedChoiceVotingApp/Candidate.cs
@@ -0,0 +1,9 @@
+﻿namespace RankedChoiceVotingApp
+{
+    public class Candidate
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int RankingId { get; set; }
+    }
+}
